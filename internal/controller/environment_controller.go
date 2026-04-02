8d5d5540c508de78622431ba20f844f243a2a84d
Andrei Matveyeu <andreimu@axis.com>
2026-04-02 15:03:07 +0200

    Fix RBAC rules in controllers and add verification test
    Change-Id: I642f324e6052098b5c2a71b916672d040dd2b7b2

--- internal/controller/environment_controller.go
+++ internal/controller/environment_controller.go
@@ -49,8 +49,12 @@ type EnvironmentReconciler struct {
 // +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=environments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=environments/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=environments/finalizers,verbs=update
+// +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=clusters,verbs=get
+// +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=environmentrequests,verbs=get
 // +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=providers,verbs=get
 // +kubebuilder:rbac:groups=etos.eiffel-community.github.io,resources=providers/status,verbs=get
+// +kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;delete
+// +kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
